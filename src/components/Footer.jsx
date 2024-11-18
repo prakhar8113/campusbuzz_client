97345aef68ac69c2cd54ba8d19eb2eaa7e6e5c0c
prakhar8113 <prakhargupta20886@gmail.com>
2024-11-19 01:06:11 +0530

    size fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,8 @@ const Footer = () => {
       <footer className="border py-10">
         {/* <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
          */}
-        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {/* <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> */}
+        <div className="container mx-auto flex flex-wrap justify-around gap-8">
           <div className=" text-center md:text-start">
             <h2 className="text-lg font-semibold mb-4">Campus Clubs</h2>
             <ul className="space-y-2">
